e92b4dae676f9d3b4ee1c3f63bb91d8adb5d1e8a
temptado <jason1287712@gmail.com>
2021-09-16 13:59:14 -0400

    added route to view single milestone and content of milestone

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,4 +14,23 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/milestone/:id', async (req, res) => {
+    try {
+        const milestoneData = await Milestone.findByPk(req.params.id);
+
+        if(!milestoneData) {
+            res.status(404).json({message: 'No milestone found with that id'});
+            return;
+        }
+
+        const milestone = milestoneData.get({plain: true});
+        console.log(milestone);
+        res.render('milestone', {
+            ...milestone
+        });
+    } catch (err) {
+        res.json(err);
+    }
+});
+
 module.exports = router;
