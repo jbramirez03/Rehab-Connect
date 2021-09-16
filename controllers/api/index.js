565e10c94ee42b89354c1f3b5c3d1767fc3c656e
temptado <jason1287712@gmail.com>
2021-09-16 12:09:30 -0400

    added user routes to api folder index

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+
+const userRoutes = require('./userRoutes');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -58,3 +58,5 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
+
+module.exports = router;
