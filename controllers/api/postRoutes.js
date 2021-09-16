69a9695baf4e8d4a39119fdd38e3ad9615254245
temptado <jason1287712@gmail.com>
2021-09-16 12:28:50 -0400

    created routes to create new posts

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            post_text: req.body.post_text,
+            user_id: req.session.user_id,
+            milestone_id: req.body.milestone_id
+        });
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id.' });
+            return;
+        }
+
+        res.status(400).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
