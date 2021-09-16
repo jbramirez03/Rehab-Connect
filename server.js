f635616f7d5b8882da18415651b2495b2214e577
temptado <jason1287712@gmail.com>
2021-09-16 12:03:50 -0400

    created .env file

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(routes);
 
