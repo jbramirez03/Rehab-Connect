fb3ecfdbea526a85d9cda982a052e9fc96904af0
Qabas al ani <doctorqa@yahoo.com>
2021-09-18 02:54:32 -0400

    added the router with auth for my data base

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,3 +2,40 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render("dashboard", { posts, loggedIn: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
