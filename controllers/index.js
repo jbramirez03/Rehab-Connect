6967ed036c1f12ebc82ffd68a09429fbb46213e7
temptado <jason1287712@gmail.com>
2021-09-20 18:50:38 -0400

    created form handler to edit user

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes');
 
 router.use('/api', apiRoutes);
