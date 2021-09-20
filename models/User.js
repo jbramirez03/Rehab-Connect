66e269792e45d7294a4d20de5ce11793408a1a67
temptado <jason1287712@gmail.com>
2021-09-20 19:28:42 -0400

    deleted update password hook

--- models/User.js
+++ models/User.js
@@ -58,15 +58,6 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-      beforeUpdate: async updatedUserData => {
-        if (updatedUserData.password) {
-          updatedUserData.password = await bcrypt.hash(
-            updatedUserData.password,
-            10
-          );
-        }
-        return updatedUserData;
-      },
     },
     sequelize,
     timestamps: false,
